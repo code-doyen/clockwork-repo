8741931f4da047f5d32a7b04b61aaf42772bd5a1
David Vollmar <david.bradley.vollmar@gmail.com>
2018-01-29 09:53:35 -0500

    Updated ClockWork.Web Index.cshtml GET request to point to the API url.  Updated Clockwork.API Debug to launch browser to correct localhost port. Ran 'dotnet ef database update' to apply the new migration to the database

--- Clockwork.Web/App_Start/RouteConfig.cs
+++ Clockwork.Web/App_Start/RouteConfig.cs
@@ -12,7 +12,8 @@ namespace Clockwork.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }//,
+               // namespaces: new string[] { "Clockwork.Web.Controllers" }
             );
         }
     }
